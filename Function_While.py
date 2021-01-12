72d664cc18c9657fe96efcff29b2f49097fb9c64
OleksiySverdlov <77245713+OleksiySverdlov@users.noreply.github.com>
2021-01-12 04:46:14 +0200

    Compare numbers method added.

--- Function_While.py
+++ Function_While.py
@@ -1,2 +1,8 @@
-class = 1
-2
+def compare_numbers(a, b, v):
+    while a <= b:
+        print("Value of A(" + str(a) + ") is less than b or equal")
+        a = a + v
+    print("Final A is " + str(a) + " waited")
+
+
+compare_numbers(1, 15, 2)
